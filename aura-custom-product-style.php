9e329b4428488bf0708f931d0ac5f9e28bb51bc2
juandsome <63100421+juandsome@users.noreply.github.com>
2025-12-02 00:53:56 -0400

    si

--- aura-custom-product-style.php
+++ aura-custom-product-style.php
@@ -3,7 +3,7 @@
  * Plugin Name: Aura Custom Product Style
  * Plugin URI: https://collectionaura.com
  * Description: Elementor widgets to display WooCommerce products related to villas in cart with multiple layout options
- * Version: 1.5.4
+ * Version: 1.5.5
  * Author: Collection Aura
  * Author URI: https://collectionaura.com
  * License: GPL v2 or later
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define plugin constants
-define( 'AURA_CPS_VERSION', '1.5.4' );
+define( 'AURA_CPS_VERSION', '1.5.5' );
 define( 'AURA_CPS_DIR', plugin_dir_path( __FILE__ ) );
 define( 'AURA_CPS_URL', plugin_dir_url( __FILE__ ) );
 define( 'AURA_CPS_BASENAME', plugin_basename( __FILE__ ) );
